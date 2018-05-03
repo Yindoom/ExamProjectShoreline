c9329fa8bb477174b7fddb971fcd90a4fc9fde41
Yindoom <31922816+Yindoom@users.noreply.github.com>
2018-05-03 11:57:15 +0200

    Converter class

--- src/examproject2/BLL/BLLManager.java
+++ src/examproject2/BLL/BLLManager.java
@@ -5,6 +5,7 @@
  */
 package examproject2.BLL;
 
+import static examproject2.BLL.Converter.filetype.xlsx;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -25,96 +26,10 @@ import org.json.simple.JSONObject;
  * @author Yindo
  */
 public class BLLManager {
-
-    public void convert(String text) throws FileNotFoundException, IOException {
-
-        List<String> headers = new ArrayList();
-        List<String> keywords = new ArrayList();
-        List<String> cells = new ArrayList();
-        //JSONObject obj = new JSONObject();
-        List<Integer> colIndex = new ArrayList();
-
-        keywords.add("Order Type");
-        keywords.add("System Status");
-        keywords.add("Opr. short text");
-        keywords.add("priority");
-        keywords.add("priority1");
-        
-        
-        List<JSONObject> myJSONObjects = new ArrayList();
-        File excel = new File(text);
-        FileInputStream fis = new FileInputStream(excel);
-        XSSFWorkbook book = new XSSFWorkbook(fis);
-        XSSFSheet sheet = book.getSheetAt(0);
-
-        Iterator<Row> itr = sheet.iterator();
-
-        while (itr.hasNext()) {
-            JSONObject obj = new JSONObject();
-            Row row = itr.next();
-
-            
-            Iterator<Cell> cellIterator = row.cellIterator();
-
-            while (cellIterator.hasNext()) {
-
-                Cell cell = cellIterator.next();
-
-                if (row.getRowNum() == 0) {
-
-                    String cellValue = cell.getStringCellValue();
-
-                    for (String key : keywords) {
-                        int n = 0;
-                        if (cellValue.toLowerCase().equals(key.toLowerCase())) {
-                            for (String header : headers) {
-                                if (header.toLowerCase().equals(cellValue.toLowerCase())) {
-                                    n++;
-                                }
-                            }
-                            if (n != 0) {
-                                cellValue = cellValue + n;
-                            }
-                        }
-                        if (cellValue.toLowerCase().equals(key.toLowerCase())) {
-                            colIndex.add(cell.getColumnIndex());
-                            headers.add(cellValue);
-                        }
-                    }
-                }
-                
-                
-                //  System.out.println(colIndex);
-                if (row.getRowNum() != 0) {
-                    for (int index : colIndex) {
-                        if (index == cell.getColumnIndex()) {
-                           
-                            switch (cell.getCellType()) {
-                                case Cell.CELL_TYPE_STRING:
-                                    System.out.println(cell.getStringCellValue());
-                                    obj.put(cell.getColumnIndex(), cell.getStringCellValue());
-                                    break;
-                                case Cell.CELL_TYPE_NUMERIC:
-                                    System.out.println(cell.getNumericCellValue());
-                                    obj.put(cell.getColumnIndex(), cell.getNumericCellValue());
-                                    break;
-                                default:
-                        
-                            }
-                        }
-                    }
-                }
-              
-            }
-            if(row.getRowNum() != 0)
-        myJSONObjects.add(obj);    
-        }
-        for (int i = 0; i < myJSONObjects.size(); i++) {
-            try (FileWriter file = new FileWriter("C:\\Users\\Yindo\\Desktop\\obj\\test"+ i+".json")) {
-            file.write(myJSONObjects.get(i).toJSONString());
-            file.flush();}
-            
-        }
-
+    
+    Converter convert = new Converter();
+    
+    public void convert(String text) throws IOException {
+        convert.convert(text, xlsx);
     }
 }
